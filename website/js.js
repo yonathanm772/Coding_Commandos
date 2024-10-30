9cdbfcebef47560df4a52816e790557fe11e17c6
jcfarese <112351396+jcfarese@users.noreply.github.com>
2024-10-30 13:30:53 -0400

    push

--- website/js.js
+++ website/js.js
@@ -20,4 +20,15 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-// End Swiper Slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
